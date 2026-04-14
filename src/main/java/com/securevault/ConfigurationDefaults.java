d7218f11f7087a9c9140b9ebc5a2f5ddf5df868d
anonymous6291 <anishkumarshaw02@gmail.com>
2026-04-15 03:02:32 +0900

    Updated files.

---
+++ src/main/java/com/securevault/ConfigurationDefaults.java
@@ -0,0 +1,19 @@
+package com.securevault;
+
+public class ConfigurationDefaults {
+    private static final Data configurationManagerData = new Data("CONFIG_KEY".toCharArray(), new byte[]{1, 2, 3, 4, 5}, new byte[]{1, 2, 3, 4, 5});
+    private static final Data loggerData = new Data(new char[0], new byte[]{1, 2, 3, 4, 5}, new byte[]{1, 2, 3, 4, 5});
+
+    public static Data getDefault(Class<?> caller) {
+        if (caller == ConfigurationManager.class) {
+            return configurationManagerData;
+        } else if (caller == Logger.class) {
+            return loggerData;
+        } else {
+            return new Data(new char[0], new byte[0], new byte[0]);
+        }
+    }
+
+    public record Data(char[] key, byte[] iv, byte[] salt) {
+    }
+}
